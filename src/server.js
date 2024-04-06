8a7b9b2ef140c22d894588826bda1ee608a60db1
leonardopottmayer <pottmayerleonardo@gmail.com>
2024-04-06 10:05:00 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+
+dotenv.config();
+const APPLICATION_PORT = process.env.APPLICATION_PORT ?? 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+
+// Create a transporter using SMTP transport
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+app.post("/send-email", (req, res) => {
+  const { to, subject, text } = req.body;
+
+  if (!to || !subject || !text) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  // Email data
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: to.toString(),
+    subject: subject.toString(),
+    text: text.toString(),
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      return res.status(400).json({ message: "Failed to send e-mail.", error });
+    } else {
+      console.log("Email sent:", info.response);
+      return res
+        .status(200)
+        .json({ message: "E-mail sent successfully!.", info });
+    }
+  });
+});
+
+app.listen(APPLICATION_PORT, () => {
+  console.log(`App started on port ${APPLICATION_PORT}.`);
+});
